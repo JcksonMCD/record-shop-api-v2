64b037fd3b81a38275cf78ee976a2ebd3033a878
Jackson MacDonald <jacksonmacdonald03@gmail.com>
2024-07-16 17:11:10 +0100

    Feat: Service layer method implementation added for getAllAlbums

--- src/main/java/com/northcoders/record_shop_api_v2/service/AlbumServiceImpl.java
+++ src/main/java/com/northcoders/record_shop_api_v2/service/AlbumServiceImpl.java
@@ -1,14 +1,20 @@
 package com.northcoders.record_shop_api_v2.service;
 
 import com.northcoders.record_shop_api_v2.model.Album;
+import com.northcoders.record_shop_api_v2.repository.AlbumRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class AlbumServiceImpl implements AlbumService{
+    AlbumRepository albumRepository;
+
     @Override
     public List<Album> getAllAlbums() {
-        return List.of();
+        List<Album> albums = new ArrayList<>();
+        albumRepository.findAll().forEach(albums::add);
+        return albums;
     }
 }
