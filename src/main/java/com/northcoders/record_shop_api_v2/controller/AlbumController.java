aea50bf607cb5d87abddc5e8c02d34fa89ea78d2
Jackson MacDonald <jacksonmacdonald03@gmail.com>
2024-07-16 17:00:58 +0100

    Feat: Service package created and Album Service added to this package

--- src/main/java/com/northcoders/record_shop_api_v2/controller/AlbumController.java
+++ src/main/java/com/northcoders/record_shop_api_v2/controller/AlbumController.java
@@ -1,9 +1,16 @@
 package com.northcoders.record_shop_api_v2.controller;
 
+import com.northcoders.record_shop_api_v2.model.Album;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/v2/album")
 public class AlbumController {
+
 }
---
+++ src/main/java/com/northcoders/record_shop_api_v2/service/AlbumServiceImpl.java
@@ -0,0 +1,7 @@
+package com.northcoders.record_shop_api_v2.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AlbumServiceImpl implements AlbumService{
+}
