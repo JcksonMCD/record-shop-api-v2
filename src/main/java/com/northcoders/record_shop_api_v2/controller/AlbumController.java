405f8372f6302ad0c0ecaee3177ad81979d7482e
Jackson MacDonald <jacksonmacdonald03@gmail.com>
2024-07-16 16:54:07 +0100

    Feat: Request Mapping pathway added

--- src/main/java/com/northcoders/record_shop_api_v2/controller/AlbumController.java
+++ src/main/java/com/northcoders/record_shop_api_v2/controller/AlbumController.java
@@ -1,7 +1,9 @@
 package com.northcoders.record_shop_api_v2.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("api/v2/album")
 public class AlbumController {
 }
