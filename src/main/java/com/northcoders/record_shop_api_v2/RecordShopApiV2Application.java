377acf32862b4f22e54bbbd8315410c2d2fa99af
Jackson MacDonald <jacksonmacdonald03@gmail.com>
2024-07-16 15:57:05 +0100

    First commit

---
+++ src/main/java/com/northcoders/record_shop_api_v2/RecordShopApiV2Application.java
@@ -0,0 +1,13 @@
+package com.northcoders.record_shop_api_v2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecordShopApiV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecordShopApiV2Application.class, args);
+	}
+
+}
