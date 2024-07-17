82288a1005ad051b649545228c0dd62abcc37234
Jackson MacDonald <jacksonmacdonald03@gmail.com>
2024-07-17 16:20:58 +0100

    Feat: Test class created for AlbumController

---
+++ src/test/java/com/northcoders/record_shop_api_v2/controller/AlbumControllerTest.java
@@ -0,0 +1,12 @@
+package com.northcoders.record_shop_api_v2.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AlbumControllerTest {
+
+    @Test
+    void getAllAlbums() {
+    }
+}
