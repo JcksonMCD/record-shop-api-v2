cbcbb7683e53cf797e3513dea430d9391f8bcc20
Jackson MacDonald <jacksonmacdonald03@gmail.com>
2024-07-22 14:16:56 +0100

    Feat: Mocked Repository added to service layer testing

--- src/test/java/com/northcoders/record_shop_api_v2/service/AlbumServiceImplTest.java
+++ src/test/java/com/northcoders/record_shop_api_v2/service/AlbumServiceImplTest.java
@@ -1,10 +1,15 @@
 package com.northcoders.record_shop_api_v2.service;
 
+import com.northcoders.record_shop_api_v2.repository.AlbumRepository;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class AlbumServiceImplTest {
+
+    @Mock
+    AlbumRepository albumRepository;
 
     @Test
     void getAllAlbums() {
