f91de19d101cb5ee5db447348cc5005445378a4c
Jackson MacDonald <jacksonmacdonald03@gmail.com>
2024-07-22 15:55:14 +0100

    Feat: All fields added to ArtistDTO class.

--- src/main/java/com/northcoders/record_shop_api_v2/dto/ArtistDTO.java
+++ src/main/java/com/northcoders/record_shop_api_v2/dto/ArtistDTO.java
@@ -1,7 +1,10 @@
 package com.northcoders.record_shop_api_v2.dto;
 
+import jakarta.persistence.Column;
 import lombok.Data;
 
 @Data
 public class ArtistDTO {
+    private long id;
+    private String name;
 }
