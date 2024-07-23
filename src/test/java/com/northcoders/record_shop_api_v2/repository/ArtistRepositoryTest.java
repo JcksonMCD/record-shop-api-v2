d6d662b4ce2137831a15c3128ed330dd28e2392c
Jackson MacDonald <jacksonmacdonald03@gmail.com>
2024-07-23 17:49:34 +0100

    Feat: Artist repository test class annotations added- DataJPATest and autoconfigure.

--- src/test/java/com/northcoders/record_shop_api_v2/repository/ArtistRepositoryTest.java
+++ src/test/java/com/northcoders/record_shop_api_v2/repository/ArtistRepositoryTest.java
@@ -1,7 +1,13 @@
 package com.northcoders.record_shop_api_v2.repository;
 
+import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@DataJpaTest
+@AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
 class ArtistRepositoryTest {
 
 }
