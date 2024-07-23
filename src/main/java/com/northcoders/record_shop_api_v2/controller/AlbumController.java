9dec9d814ac33b50f8f86a7c1781b8a29d11d99e
Jackson MacDonald <jacksonmacdonald03@gmail.com>
2024-07-23 13:51:54 +0100

    Feat: Post Album method added to controller layer.

--- src/main/java/com/northcoders/record_shop_api_v2/controller/AlbumController.java
+++ src/main/java/com/northcoders/record_shop_api_v2/controller/AlbumController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,6 +27,11 @@ public class AlbumController {
     @GetMapping
     public ResponseEntity<List<AlbumDTO>> getAllAlbums(){
         return new ResponseEntity<>(albumService.getAllAlbums(), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<AlbumDTO> postAlbum(){
+        return new ResponseEntity<>(albumService.postAlbum(), HttpStatus.CREATED);
     }
 
 }
