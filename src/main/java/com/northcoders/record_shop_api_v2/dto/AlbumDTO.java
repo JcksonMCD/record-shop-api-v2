35a932989801dd824df40e434e33bfbeef44452d
Jackson MacDonald <jacksonmacdonald03@gmail.com>
2024-07-23 13:10:16 +0100

    Feat: Test for Controller Get request added.

--- src/main/java/com/northcoders/record_shop_api_v2/dto/AlbumDTO.java
+++ src/main/java/com/northcoders/record_shop_api_v2/dto/AlbumDTO.java
@@ -1,9 +1,15 @@
 package com.northcoders.record_shop_api_v2.dto;
 
 import com.northcoders.record_shop_api_v2.model.Genre;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class AlbumDTO {
     private long id;
     private String albumName;
--- src/test/java/com/northcoders/record_shop_api_v2/controller/AlbumControllerTest.java
+++ src/test/java/com/northcoders/record_shop_api_v2/controller/AlbumControllerTest.java
@@ -1,43 +1,64 @@
 package com.northcoders.record_shop_api_v2.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.northcoders.record_shop_api_v2.dto.AlbumDTO;
 import com.northcoders.record_shop_api_v2.model.Album;
 import com.northcoders.record_shop_api_v2.model.Genre;
-import com.northcoders.record_shop_api_v2.service.AlbumServiceImpl;
-import lombok.Setter;
+import com.northcoders.record_shop_api_v2.service.AlbumService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(controllers = AlbumController.class)
 @AutoConfigureMockMvc(addFilters = false)
 @ExtendWith(MockitoExtension.class)
 class AlbumControllerTest {
 
-    @Mock
-    AlbumServiceImpl albumService;
+    @Autowired
+    MockMvc mockMvc;
 
-    @InjectMocks
-    AlbumController albumController;
+    @MockBean
+    AlbumService albumService;
 
+    @Autowired
+    ObjectMapper objectMapper;
+
+    private Album album;
+    private AlbumDTO albumDTO;
+
+    @BeforeEach
+    public void init(){
+        album = Album.builder().albumName("Nothing But Thieves").genre(Genre.ROCK).releaseYear(2020).build();
+        albumDTO = AlbumDTO.builder().albumName("Nothing But Thieves").genre(Genre.ROCK).releaseYear(2020).build();
+    }
 
     @Test
     @DisplayName("GET /albums: Returns all albums")
-    void getAllAlbums() {
+    void AlbumRepository_GetAll_ReturnsAllAlbumDTOs() throws Exception {
+        List<AlbumDTO> responseDTOList = List.of(albumDTO);
+        when(albumService.getAllAlbums()).thenReturn(responseDTOList);
 
+        ResultActions response = mockMvc.perform(get("/api/v2/album"));
+
+        response
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].albumName").value(albumDTO.getAlbumName()))
+                .andExpect(jsonPath("$[0].genre").value(albumDTO.getGenre().toString()))
+                .andExpect(jsonPath("$[0].releaseYear").value(albumDTO.getReleaseYear()));
     }
 }
