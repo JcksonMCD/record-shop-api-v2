991c005088f91749479c2a341d3ef71c7721053e
Jackson MacDonald <jacksonmacdonald03@gmail.com>
2024-07-23 17:48:13 +0100

    Feat: Artist repository test class created.

--- src/main/java/com/northcoders/record_shop_api_v2/repository/ArtistRepository.java
+++ src/main/java/com/northcoders/record_shop_api_v2/repository/ArtistRepository.java
@@ -4,4 +4,5 @@ import com.northcoders.record_shop_api_v2.model.Artist;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ArtistRepository extends JpaRepository<Artist, Long> {
+
 }
