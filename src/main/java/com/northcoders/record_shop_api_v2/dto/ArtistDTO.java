351fda4334cd1bf0d2d4433a08fe8fdd703ec2e1
Jackson MacDonald <jacksonmacdonald03@gmail.com>
2024-07-23 13:21:00 +0100

    Feat: Service impl test passed for getAll() method.

--- src/main/java/com/northcoders/record_shop_api_v2/dto/ArtistDTO.java
+++ src/main/java/com/northcoders/record_shop_api_v2/dto/ArtistDTO.java
@@ -2,8 +2,10 @@ package com.northcoders.record_shop_api_v2.dto;
 
 import jakarta.persistence.Column;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 public class ArtistDTO {
     private long id;
     private String name;
--- src/test/java/com/northcoders/record_shop_api_v2/service/AlbumServiceImplTest.java
+++ src/test/java/com/northcoders/record_shop_api_v2/service/AlbumServiceImplTest.java
@@ -1,14 +1,23 @@
 package com.northcoders.record_shop_api_v2.service;
 
 import com.northcoders.record_shop_api_v2.dto.AlbumDTO;
+import com.northcoders.record_shop_api_v2.dto.ArtistDTO;
+import com.northcoders.record_shop_api_v2.model.Album;
+import com.northcoders.record_shop_api_v2.model.Artist;
+import com.northcoders.record_shop_api_v2.model.Genre;
 import com.northcoders.record_shop_api_v2.repository.AlbumRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class AlbumServiceImplTest {
@@ -19,8 +28,24 @@ class AlbumServiceImplTest {
     @InjectMocks
     private AlbumServiceImpl albumService;
 
+    private Album album;
+    private AlbumDTO albumDTO;
+
+    @BeforeEach
+    public void init(){
+        MockitoAnnotations.openMocks(this);
+        album = Album.builder().albumName("Nothing But Thieves").artist(new Artist()).genre(Genre.ROCK).releaseYear(2020).build();
+        albumDTO = AlbumDTO.builder().albumName("Nothing But Thieves").artist(new ArtistDTO()).genre(Genre.ROCK).releaseYear(2020).build();
+
+    }
+
     @Test
     void AlbumService_GetAll_ReturnsResponseDTO() {
+        when(albumRepository.findAll()).thenReturn(List.of(album));
 
+        List<AlbumDTO> expectedDTOList = List.of(albumDTO);
+        List<AlbumDTO> actualDTOList = albumService.getAllAlbums();
+
+        assertEquals(expectedDTOList, actualDTOList);
     }
 }
