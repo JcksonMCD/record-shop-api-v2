bcadbb1b8afa8b5b1c440d7b103fedf0a4e221c4
Jackson MacDonald <jacksonmacdonald03@gmail.com>
2024-07-24 21:08:35 +0100

    Feat: Caching enabled in main.

--- src/main/java/com/northcoders/record_shop_api_v2/RecordShopApiV2Application.java
+++ src/main/java/com/northcoders/record_shop_api_v2/RecordShopApiV2Application.java
@@ -2,9 +2,11 @@ package com.northcoders.record_shop_api_v2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 
 @SpringBootApplication
+@EnableCaching
 public class RecordShopApiV2Application {
 
 	public static void main(String[] args) {
