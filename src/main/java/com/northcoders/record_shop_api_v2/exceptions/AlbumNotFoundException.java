1b124d913b331168d20dfc8bc1473dd7f542d80e
Jackson MacDonald <jacksonmacdonald03@gmail.com>
2024-07-24 16:03:05 +0100

    Feat: Error Object class created for clearer exception handling on the user side. This object will be used to neatly lay out error information in a clear and readable format.

--- src/main/java/com/northcoders/record_shop_api_v2/exceptions/AlbumNotFoundException.java
+++ src/main/java/com/northcoders/record_shop_api_v2/exceptions/AlbumNotFoundException.java
@@ -1,4 +1,12 @@
 package com.northcoders.record_shop_api_v2.exceptions;
 
-public class AlbumNotFoundException {
+import java.io.Serial;
+
+public class AlbumNotFoundException extends RuntimeException{
+    @Serial
+    private static final long serialVersionUID = 1;
+
+    public AlbumNotFoundException(String message) {
+        super(message);
+    }
 }
---
+++ src/main/java/com/northcoders/record_shop_api_v2/exceptions/ErrorObject.java
@@ -0,0 +1,7 @@
+package com.northcoders.record_shop_api_v2.exceptions;
+
+import lombok.Data;
+
+@Data
+public class ErrorObject {
+}
