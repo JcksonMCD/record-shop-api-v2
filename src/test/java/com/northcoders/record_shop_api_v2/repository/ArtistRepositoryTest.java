338db5c3557781c97898d9ee18576c85b9a523f3
Jackson MacDonald <jacksonmacdonald03@gmail.com>
2024-07-24 11:33:26 +0100

    Feat: Test class set up complete for Artist repository.

--- src/test/java/com/northcoders/record_shop_api_v2/repository/ArtistRepositoryTest.java
+++ src/test/java/com/northcoders/record_shop_api_v2/repository/ArtistRepositoryTest.java
@@ -1,13 +1,35 @@
 package com.northcoders.record_shop_api_v2.repository;
 
+import com.northcoders.record_shop_api_v2.model.Album;
+import com.northcoders.record_shop_api_v2.model.Genre;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import com.northcoders.record_shop_api_v2.model.Artist;
+
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
 class ArtistRepositoryTest {
+
+    @Autowired
+    ArtistRepository artistRepository;
+
+    private Artist artist;
+
+    @BeforeEach
+    public void init(){
+        artist = new Artist(1L ,"Test Artist", null);
+    }
+
+    @AfterEach
+    public void tearDown() {
+        artistRepository.deleteAll();
+    }
 
 }
