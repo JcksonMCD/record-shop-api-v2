9132f6bddd15b467d7bef1b5ae7749c56a21e17a
Jackson MacDonald <jacksonmacdonald03@gmail.com>
2024-07-24 16:06:11 +0100

    Feat: Fields added to error object.

--- src/main/java/com/northcoders/record_shop_api_v2/exceptions/ErrorObject.java
+++ src/main/java/com/northcoders/record_shop_api_v2/exceptions/ErrorObject.java
@@ -2,6 +2,10 @@ package com.northcoders.record_shop_api_v2.exceptions;
 
 import lombok.Data;
 
+// Class used to package error information in a clear and readable format. Improves user experience.
 @Data
 public class ErrorObject {
+    private Integer statusCode;
+    private String message;
+    private Data timeStamp;
 }
