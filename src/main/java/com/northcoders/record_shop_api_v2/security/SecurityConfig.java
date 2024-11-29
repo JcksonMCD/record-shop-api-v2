d91990490349096fc92cfc0c6653ced29e6ad546
Jackson MacDonald <jacksonmacdonald03@gmail.com>
2024-11-29 21:01:34 +0000

    Feat: Filter chain method signiture added to security config class.

--- src/main/java/com/northcoders/record_shop_api_v2/security/SecurityConfig.java
+++ src/main/java/com/northcoders/record_shop_api_v2/security/SecurityConfig.java
@@ -1,7 +1,15 @@
 package com.northcoders.record_shop_api_v2.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http){
+
+    }
 }
