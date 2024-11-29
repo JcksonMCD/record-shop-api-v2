0824726e4b394894474342685440c25fcb98c353
Jackson MacDonald <jacksonmacdonald03@gmail.com>
2024-11-29 21:21:18 +0000

    Feat: Security config set up.

--- src/main/java/com/northcoders/record_shop_api_v2/security/SecurityConfig.java
+++ src/main/java/com/northcoders/record_shop_api_v2/security/SecurityConfig.java
@@ -2,14 +2,26 @@ package com.northcoders.record_shop_api_v2.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http){
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .headers(headers -> headers.frameOptions().disable())
+                .authorizeHttpRequests(auth -> {
+                    auth.requestMatchers(HttpMethod.GET).permitAll()
+                            .anyRequest().authenticated();
+                })
+                .oauth2Client(Customizer.withDefaults());
 
+        return http.build();
     }
 }
